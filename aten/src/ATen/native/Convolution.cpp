7fe777090fe7b3220a22ff518922f2d0a19d1050
xiaobing.zhang <xiaobing.zhang@intel.com>
2019-10-18 14:47:12 +0800

    enable mkldnn conv3d

--- aten/src/ATen/native/Convolution.cpp
+++ aten/src/ATen/native/Convolution.cpp
@@ -182,9 +182,7 @@ auto ConvParams::use_mkldnn(const at::Tensor& input) const -> bool {
   return (input.is_mkldnn()) || // input is mkldnn Tensor
     (input.type().backend() == at::Backend::CPU &&
      input.scalar_type() == kFloat && // only on CPU Float Tensors
-     !is_dilated() && // doesn't support dilation
-     !transposed && // or transposed tensors
-     input.ndimension() == 4); // must be in NCHW format
+     !transposed); // must be in NCHW format
 #endif
   return false;
 }
